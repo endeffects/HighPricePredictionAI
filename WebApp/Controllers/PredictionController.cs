ad11181da63f6b890a722de9422a5eede65637bc
hs115488 <haresh.singh.97@gmail.com>
2020-05-08 04:25:49 -0400

    FINISHED

---
+++ WebApp/Controllers/PredictionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using WebAppML.Model;
+
+namespace WebApp.Controllers
+{
+    public class PredictionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Predict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Predict(ModelInput input)
+        {
+            var prediction = ConsumeModel.Predict(input);
+            ViewBag.Result = prediction;
+
+            return View();
+        }
+    }
+}
